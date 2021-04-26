fa4470f67afab774ea885caa001b4d7f8bd044d5
HR Williams <halrubinwilliams@gmail.com>
2021-04-26 15:41:56 -0700

    Add non compiling test for description property

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -13,6 +13,13 @@ namespace PierresBakery.Tests
       Bread newBread = new Bread();
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
-
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "Pierre's Loaf";
+      Bread newBread = new Bread(description);
+      string result = newBread.Description;
+      Assert.AreEqual(description, result);
+    }
   }
 }
