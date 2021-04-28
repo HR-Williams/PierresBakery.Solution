f794b74cd96647fca0cbc00ff53c4a297bebdf46
HR Williams <halrubinwilliams@gmail.com>
2021-04-28 16:43:26 -0700

    Declare static variable that maintains a list of all Bread objects

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -1,12 +1,20 @@
+using System.Collections.Generic;
+
 namespace PierresBakery.Models
 {
   public class Bread
   {
     public string Description { get; }
-
+    private static List<Bread> _instances = new List<Bread> {};
+    public static List<Bread> GetAll()
+    {
+    return _instances;
+    }
     public Bread(string description)
     {
       Description = description;
+      _instances.Add(this);
+      
     }
   }
 }
