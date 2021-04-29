2da550a7c454e61c0454ec57ff81c44a0810210f
HR Williams <halrubinwilliams@gmail.com>
2021-04-28 17:06:30 -0700

    Declare static variable that maintains a list of all Pastry objects and clear method

--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -1,12 +1,21 @@
+using System.Collections.Generic;
+
 namespace PierresBakery.Models
 {
   public class Pastry
   {
     public string Description { get; }
 
+    private static List<Pastry> _instances = new List<Pastry> {};
+
+    public static List<Pastry> GetAll()
+    {
+    return _instances;
+    }
     public Pastry(string description)
     {
       Description = description;
+      _instances.Add(this);
     }
   }
 }
