486c757eec4098394ce4a44a5f1991fdc1a729e4
HR Williams <halrubinwilliams@gmail.com>
2021-04-28 17:10:48 -0700

    Add clear all method

--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -12,6 +12,11 @@ namespace PierresBakery.Models
     {
     return _instances;
     }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
     public Pastry(string description)
     {
       Description = description;
