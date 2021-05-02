7da42c987f937108d71eb2201880d2542e070a13
HR Williams <halrubinwilliams@gmail.com>
2021-05-02 08:31:25 -0700

    Add program.cs with Pastry and Bread objects

---
+++ PierresBakery/Program.cs
@@ -0,0 +1,15 @@
+using System;
+// using System.Collections.Generic;
+using PierresBakery.Models;
+
+namespace PierresBakery {
+  public class Program
+  {
+    public void Main()
+    {
+      Pastry pierresPastry = new Pastry(2);
+
+      Bread pierresLoaf = new Bread(5);
+    }
+  }  
+}
