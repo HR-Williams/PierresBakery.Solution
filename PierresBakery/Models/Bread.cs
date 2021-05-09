6721210c7e1145d33e1b867788df139e4d05db57
HR Williams <halrubinwilliams@gmail.com>
2021-05-08 21:57:09 -0700

    Updates bread model, variable names, adds BreadQuantity

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -1,28 +1,24 @@
 using System.Collections.Generic;
+using System;
 
 namespace PierresBakery.Models
 {
   public class Bread
   {
-    // public string Description { get; }
+      public int BreadPrice { get; set; } = 5;
+      public int BreadQuantity {get; set;}
 
-      public int Price { get; }
+      public Bread(int breadQuantity) 
+      {
+      BreadQuantity = breadQuantity;
+      // Price = 5;
+      }
 
-      public Bread(int price) 
-    {
-      Price = 5;
-      _instances.Add(this);
-    }
-
-    private static List<Bread> _instances = new List<Bread> {};
-    public static List<Bread> GetAll()
-    {
-    return _instances;
-    }
-
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
+    // public int TotalBreadPrice()
+    // {
+    //   //seeing if there are three or more
+    //   //calculating quantity + price
+    //   //return total price  
+    // }
   }
 }
